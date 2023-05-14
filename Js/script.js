3d338c0a4ff7d160373fca10a956c8b617c2b7cb
Tommy007U <tozakitoko2@gmail.com>
2023-05-14 16:29:43 +0200

    orgainizing javascript & css files

--- Js/script.js
+++ Js/script.js
@@ -1,27 +1,17 @@
 function playSound(e){
-
-    
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);// this code will select the sound allocateted to a specific data-key via the given number (`audio[data-key ="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); 
-    if(!audio)return; // if no audio stop the function all running together
-    audio.currentTime = 0; //rewind to the start
-
+    if(!audio)return; 
+    audio.currentTime = 0; 
     audio.play()
-    key.classList.add('playing'); // To understand the role of this new cladd .playin go on css and modify >playing modify transform.scale > on key modify transition all to view well
-
-    //console.log(key); // on browser while the key is press down the consle will print the key element pressed
-    //console.log(audio); // here we test the console on the browser to view the outcome accordig the specific allocation
+    key.classList.add('playing'); 
  }
 
-
 function removeTransition(e){
-    if(e.propertyName !== 'transform') return; // skip if it's not a transform 
+    if(e.propertyName !== 'transform') return;
     this.classList.remove('.playing');
-
 }
-
-const keys = document.querySelectorAll('.key') // will select all the key class on the page
-//console.log('.key')
+const keys = document.querySelectorAll('.key') 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
